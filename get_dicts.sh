6f6f5fe9dd9e7f3dfcda3e6707d6b0475b62264c
janhun-ec <janosi.hunor@encosoft.hu>
2024-12-13 16:56:06 +0100

    refactor get_dicts.sh

--- get_dicts.sh
+++ get_dicts.sh
@@ -3,17 +3,19 @@ mkdir -p dicts
 cd dicts
 
 # for libreoffice
-langs=('en' 'hu' 'ro' 'ko' 'de' 'fr' 'pl' 'pt' 'es' 'it' 'ru' 'sl' 'tr' 'sk' 'cs' 'ar')
-languages=('en' 'hu_HU' 'ro' 'ko_KR' 'de' 'fr_FR' 'pl_PL' 'pt_PT' 'es' 'it_IT' 'ru_RU' 'sl_SI' 'tr_TR' 'sk_SK' 'cs_CZ' 'ar')
-languagesLong=('en_US' 'hu_HU' 'ro_RO' 'ko_KR' 'de_DE_frami' 'fr' 'pl_PL' 'pt_PT' 'es_ES' 'it_IT' 'ru_RU' 'sl_SI' 'tr_TR' 'sk_SK' 'cs_CZ' 'ar')
+lang_codes=('en' 'hu' 'ro' 'ko' 'de' 'fr' 'pl' 'pt' 'es' 'it' 'ru' 'sl' 'tr' 'sk' 'cs' 'ar')
+lang_dirs=('en' 'hu_HU' 'ro' 'ko_KR' 'de' 'fr_FR' 'pl_PL' 'pt_PT' 'es' 'it_IT' 'ru_RU' 'sl_SI' 'tr_TR' 'sk_SK' 'cs_CZ' 'ar')
+lang_files=('en_US' 'hu_HU' 'ro_RO' 'ko_KR' 'de_DE_frami' 'fr' 'pl_PL' 'pt_PT' 'es_ES' 'it_IT' 'ru_RU' 'sl_SI' 'tr_TR' 'sk_SK' 'cs_CZ' 'ar')
 # XXX ZH
 
 extensions=('dic' 'aff')
 
 # download each .dic and .aff file for each language
-for i in "${!languages[@]}"; do
-    echo curling "${languages[i]}" "${languagesLong[i]}"
+for i in "${!lang_codes[@]}"; do
+    echo curling "${lang_codes[i]}"
     for extension in "${extensions[@]}"; do
-        curl -o "${langs[i]}"."$extension" "https://raw.githubusercontent.com/LibreOffice/dictionaries/refs/heads/master/${languages[i]}/${languagesLong[i]}.$extension"
+        outputfile="${lang_codes[i]}.${extension}"
+        url="https://raw.githubusercontent.com/LibreOffice/dictionaries/refs/heads/master/${lang_dirs[i]}/${lang_files[i]}.$extension"
+        curl -o "$outputfile" "$url"
     done
 done
